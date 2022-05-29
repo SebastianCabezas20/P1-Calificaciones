7b282835bc39574080709358d5971f296ef43092
Miguel Diaz <miguel.diaz.ar@usach.cl>
2022-05-29 19:40:19 -0400

    Funcionalidad en el dashboard del Docente

--- frontend/src/views/Docente/DashboardDocente.vue
+++ frontend/src/views/Docente/DashboardDocente.vue
@@ -9,7 +9,19 @@
 
   <div class="contentViews">
     <div class="centralContent">
-      <p>Contenido aquí del docente...</p>
+      <div class="row" style="background-color: #ffffff">
+        <div class="col-md-3" id="solPendientes" @click="solicitudes()">
+          <!-- Icono -->
+          <div class="iconoDashboard">
+            <span class="fa-solid fa-file-circle-exclamation fa-3x"></span>
+          </div>
+          <!-- Texto -->
+          <h2>Solicitudes de revisión pendientes</h2>
+          <!-- Dato -->
+          <h3>{{ solicitudesPendientes }} - {{ solicitudesTotales }}</h3>
+        </div>
+        <div class="col-md"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,11 +29,46 @@
 <script>
 import Sidebar from "../../components/SidebarDocente.vue";
 import Navbar from "../../components/NavbarGeneral.vue";
+import axios from "axios";
 
 export default {
   components: {
     Sidebar,
     Navbar,
+  },
+  data() {
+    return {
+      solicitudesPendientes: 0,
+      solicitudesTotales: 0,
+    };
+  },
+  mounted() {
+    let identificacionUsuario = this.$store.getters.idUsuario;
+    let that = this;
+    axios
+      .get(`http://localhost:8000/api/docente/${identificacionUsuario}`)
+      .then(function (response) {
+        let idDocente = response.data.id;
+        axios
+          .get(`http://localhost:8000/solicitudesDocente/${idDocente}`)
+          .then(function (responseTwo) {
+            let solicitudes = responseTwo.data;
+            // Obtención núm. de solicitudes totales.
+            that.solicitudesTotales = solicitudes.length;
+
+            // Obtención núm. de solicitudes pendientes.
+            for (var i = 0; i < solicitudes.length; i++) {
+              if (solicitudes[i].estado == "P") {
+                that.solicitudesPendientes++;
+              }
+            }
+          });
+      });
+  },
+  methods: {
+    solicitudes() {
+      this.$router.push({ name: "Apelacionesdocente" });
+    },
   },
 };
 </script>
